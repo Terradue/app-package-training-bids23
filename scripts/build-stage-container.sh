3c03a254450e76fe135fb1cde692eb7ce5ba92ab
Fabrice Brito <fabrice.brito@terradue.com>
2023-10-30 14:33:58 +0100

    changes

--- scripts/build-stage-container.sh
+++ scripts/build-stage-container.sh
@@ -4,4 +4,4 @@ podman \
     build \
     --format docker \
     -t localhost/stage:latest \
-    ${WORKSPACE}/water-bodies/command-line-tools/stage-in
+    ${WORKSPACE}/water-bodies/command-line-tools/stage
