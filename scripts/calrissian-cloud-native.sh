6856d5f78258ddc570100e4e16674a58ce097aab
Fabrice Brito <fabrice.brito@terradue.com>
2023-10-31 13:45:13 +0100

    calrissian

--- scripts/calrissian-cloud-native.sh
+++ scripts/calrissian-cloud-native.sh
@@ -1,13 +1,15 @@
+mkdir -p /calrissian/logs
+
 calrissian \
     --stdout /calrissian/results.json \
     --stderr /calrissian/app.log \
-    --max-ram 20G \
-    --max-cores "20" \
+    --max-ram 4G \
+    --max-cores "8" \
     --tmp-outdir-prefix /calrissian/tmp \
     --outdir /calrissian/results \
     --usage-report /calrissian/usage.json \
     --tool-logs-basepath /calrissian/logs \
-    app-water-bodies-cloud-native.$version.cwl#water_bodies \
+    app-water-bodies-cloud-native.$version.cwl#main \
     --stac_items "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs/items/S2A_10TFK_20210708_0_L2A" \
     --stac_items "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs/items/S2B_10TFK_20210713_0_L2A" \
     --stac_items "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs/items/S2A_10TFK_20210718_0_L2A" \
--- scripts/download-cwl.sh
+++ scripts/download-cwl.sh
@@ -1,3 +1,3 @@
-version="1.4.1"
+version="1.0.0"
 
 wget https://github.com/Terradue/app-package-training-bids23/releases/download/$version/app-water-bodies-cloud-native.$version.cwl
