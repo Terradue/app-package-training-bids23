ab56c8c2b285365656538b7639ae675d03cae18f
Fabrice Brito <fabrice.brito@terradue.com>
2023-10-31 11:19:01 +0100

    cwl cli

--- scripts/cwl-cli-norm-diff.sh
+++ scripts/cwl-cli-norm-diff.sh
@@ -3,7 +3,7 @@ export WORKSPACE=/workspace/app-package-training-bids23
 cwltool \
     --podman \
     --outdir /workspace/runs \
-    ${WORKSPACE}/cwl-cli/norm_diff.cwl \
+    ${WORKSPACE}/cwl-cli/norm-diff.cwl \
     --rasters \
     /workspace/runs/crop_green.tif \
     --rasters \
