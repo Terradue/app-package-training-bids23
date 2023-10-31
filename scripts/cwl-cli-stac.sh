e029136a1121b0f156e3f5834ae4d63ef1727e2a
Fabrice Brito <fabrice.brito@terradue.com>
2023-10-31 11:20:51 +0100

    cwl cli

--- scripts/cwl-cli-stac.sh
+++ scripts/cwl-cli-stac.sh
@@ -4,5 +4,5 @@ cwltool \
     ${WORKSPACE}/cwl-cli/stac.cwl \
     --item \
     "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs/items/S2B_10TFK_20210713_0_L2A" \
-    --rasters \
+    --raster \
     /workspace/runs/otsu.tif
